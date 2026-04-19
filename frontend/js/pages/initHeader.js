49bb3098ac94e8b4871244cf4c1f277e48231572
ddavidd27 <d00479143@utahtech.edu>
2026-04-19 20:56:35 +0200

    menu hambuerguesa

--- frontend/js/pages/initHeader.js
+++ frontend/js/pages/initHeader.js
@@ -6,5 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
   if (token && chatLink) {
     chatLink.style.display = "inline-flex";
   }
+
+    const menuToggle = document.getElementById("menuToggle");
+    const mainNav = document.getElementById("mainNav");
+
+    if (menuToggle && mainNav) {
+    menuToggle.addEventListener("click", () => {
+        mainNav.classList.toggle("open");
+    });
+    }
   setupLoginLink();
 });
